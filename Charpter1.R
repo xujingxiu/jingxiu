ff2444358625fe519fa372edfbe3b7ccf7dfd791
xujingxiu <75660016+xujingxiu@users.noreply.github.com>
2021-07-10 11:59:58 +0800

    Update Charpter1.R

--- Charpter1.R
+++ Charpter1.R
@@ -81,4 +81,304 @@ library(vioplot)
 vioplot(hb,horizontal=TRUE, col="gray")
 title("Violin plot,Modern Englishman")
 boxplot(hb,horizontal=TRUE,main="Boxplot, Modern Englishman",xlab="head breadth(mm)")
-library(ggplot2)
+
+
+income<-c(7,1110,7,5,8,12,0,5,2,2,46,7)
+income<-sort(income,decreasing=TRUE)
+income
+
+summary (income)
+
+stem (income)
+
+income2<-income[-c(1,2)]
+income2
+
+summary(income2)
+
+stem(income2)
+
+stem(income2,scale=2)
+
+#### income-boxplot
+# boxplot usig R base graphics
+# 1 row, 3 columns
+par(mfrow=c(1,3))
+boxplot(income,main="income")
+boxplot(income[-1],main="(remove largest)")
+boxplot(income2,main="(remove2 largest)")
+
+x1<-rnorm(250,mean=100,sd=15)
+par(mfrow=c(3,1))
+# Histogram overlaid with kernel density curve
+hist(x1,freq=FALSE,breaks=20)
+points(density(x1),type="l")
+rug(x1)
+
+library(vioplot)
+vioplot(x1,horizontal=TRUE,col="gray")
+boxplot(x1,horizontal=TRUE)
+
+## ggplot
+# Histogram overlaid with kernel, density curve
+x1_df<-data.frame(x1)
+p1<-ggplot(x1_df,aes(x=x1))
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=5,colour="black",fill="white")
+
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(heigh=0.0005),alpha=1/5)
+
+# violin plot
+p2<-ggplot(x1_df,aes(x="x1",y=x1))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+
+# boxplot
+P3<-ggplot(x1_df,aes(x="x1",y=x1))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+library(gridExtra)
+grid.arrange(p1,p2,p3,ncol=1)
+
+library(moments)
+
+summary(x1)
+
+sd(x1)
+
+skewness(x1)
+
+kurtosis(x1)
+
+stem(x1)
+
+#### Unimodal, symmetric, heavy-tailed
+# sample from normal distribution
+x2.temp<-rnorm(250,mean=0,sd=1)
+x2<-sign(x2.temp)*x2.temp-2*15+100
+
+par(mfrow=c(3,1))
+
+# Histogram overlaid with kernel density curve
+hist(x2,freq=FALSE,breaks=20)
+points(density(x2),type="l")
+rug(x2)
+
+# violin plot
+library(vioplot)
+vioplot(x2,horizontal=TRUE,col="gray")
+# boxplot
+boxplot(x2,horizontal=TRUE)
+# Histogram overlaid with kernel, density curve
+x2_df<-data.frame(x2)
+p1<-ggplot(x2_df,aes(x=x2))
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=5,colour="black",fill="white")
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(height=0.0005),alpha=1/5)
+# violin plot
+p2<-ggplot(x2_df,aes(x="x2",y=x2))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+p3<-ggplot(x2_df,aes(x="x2",y=x2))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+library(gridExtra)
+grid.arrange(p1,p2,p3,ncol=1)
+
+summary(x2)
+
+sd(x2)
+
+skewness(x2)
+
+kurtosis(x2)
+
+stem(x2)
+
+#### Symmetric, (uniform,) short-tailed
+# sample from normal distribution
+x3<-runif(250,min=50,max=150)
+
+par(mfrow=c(3,1))
+
+# Histogram overlaid with kernel density curve
+hist(x3,freq=FALSE,breaks=20)
+points(density(x3),type="l")
+rug(x3)
+
+# violin plot
+library(vioplot)
+vioplot(x3,horizontal=TRUE,col="gray")
+# boxplot
+boxplot(x3,horizontal=TRUE)
+
+# Histogram overlaid with kernel, density curve
+x3_df<-data.frame(x3)
+p1<-ggplot(x3_df,aes(x=x3))
+
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=5,colour="black",fill="white")
+
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(height=0.0005),alpha=1/5)
+
+# violin plot
+p2<-ggplot(x3_df,aes(x="x3",y=x3))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+
+p3<-ggplot(x3_df,aes(x="x3",y=x3))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+
+library(gridExtra)
+grid.arrange(p1, p2, p3, ncol=1)
+
+summary(x3)
+
+sd(x3)
+
+skewness(x3)
+
+kurtosis(x3)
+
+stem(x3)
+
+#### Unimodal, skewed right
+# sample from normal distribution
+x4<-rexp(250,rate=1)
+par(mfrow=c(3,1))
+# Histogram overlaid with kernel density curve
+hist(x4,freq=FALSE,breaks=20)
+points(density(x4),type="l")
+rug(x4)
+
+# violin plot
+library(vioplot)
+vioplot(x4,horizontal=TRUE,col="gray")
+# boxplot
+boxplot(x4,horizontal=TRUE)
+# Histogram overlaid with kernel, density curve
+x4_df<-data.frame(x4)
+p1<-ggplot(x4_df,aes(x=x4))
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=0.5,colour="black",fill="white")
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(height=0.0005),alpha=1/5)
+# violin plot
+p2<-ggplot(x4_df,aes(x="x4",y=x4))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+p3<-ggplot(x4_df,aes(x="x4",y=x4))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+library(gridExtra)
+grid.arrange(p1,p2,p3,ncol=1)
+
+summary(x4)
+
+sd(x4)
+
+skewness(x4)
+
+kurtosis(x4)
+
+stem(x4)
+
+#### Unimodal, skewed left
+# sample from normal distribution
+x5<-15-rexp(250,rate=0.5)
+par(mfrow=c(3,1))
+# Histogram overlaid with kernel density curve
+hist(x5,freq=FALSE,breaks=20)
+points(density(x5),type="l")
+rug(x5)
+
+# violin plot
+library(vioplot)
+vioplot(x5,horizontal=TRUE,col="gray")
+# boxplot
+boxplot(x5,horizontal=TRUE)
+# Histogram overlaid with kernel, density curve
+x5_df<-data.frame(x5)
+p1<-ggplot(x5_df,aes(x=x5))
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=0.5,colour="black",fill="white")
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(height=0.0005),alpha=1/5)
+# violin plot
+p2<-ggplot(x5_df,aes(x="x5",y=x5))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+p3<-ggplot(x5_df,aes(x="x5",y=x5))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+library(gridExtra)
+grid.arrange(p1, p2, p3, ncol=1)
+
+summary(x5)
+
+sd(x5)
+
+skewness(x5)
+
+kurtosis(x5)
+
+stem(x5)
+
+#### Bimodal(muiti-modal)
+# sample from normal distribution
+x6<-c(rnorm(150,mean=100,sd=15),rnorm(150,mean=150,sd=15))
+par(mfrow=c(3, 1))
+# Histogram overlaid with kernel density curve
+hist(x6,freq=FALSE,breaks=20)
+points(density(x6),type="l")
+rug(x6)
+
+# violin plot
+library(vioplot)
+vioplot(x6,horizontal=TRUE,col="gray")
+# boxplot
+boxplot(x6,horizontal=TRUE)
+# Histogram overlaid with kernel, density curve
+x6_df<-data.frame(x6)
+p1<-ggplot(x6_df,aes(x=x6))
+# Histogram with density instead of count on y-axis
+p1<-p1+geom_histogram(aes(y=..density..),binwidth=0.5,colour="black",fill="white")
+# Overlay with transparent density plot
+p1<-p1+geom_density(alpha=0.1,fill="#FF6666")
+p1<-p1+geom_point(aes(y=-0.001),position=position_jitter(height=0.0005),alpha=1/5)
+# violin plot
+p2<-ggplot(x6_df,aes(x="x6",y=x6))
+p2<-p2+geom_violin(fill="gray50")
+p2<-p2+geom_boxplot(width=0.2,alpha=3/4)
+p2<-p2+coord_flip()
+# boxplot
+p3<-ggplot(x6_df,aes(x="x6",y=x6))
+p3<-p3+geom_boxplot()
+p3<-p3+coord_flip()
+library(gridExtra)
+grid.arrange(p1,p2,p3,ncol=1)
+
+summary(x6)
+
+sd(x6)
+
+skewness(x6)
+
+kurtosis(x6)
+
+stem(x6)
+
